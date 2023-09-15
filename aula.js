f5dae6692e5b30deab2d71fc1edecee7efbd4ace
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-15 11:48:18 -0300

    Atualiação da aula

---
+++ aula.js
@@ -0,0 +1,10 @@
+// Criando um alert em JavaScript
+alert("Olá mundo!")
+// Adicionando uma mensagem na tela
+document.write("Olá mundo!"); 
+
+// Adicionando console.log
+console.log("Olá 123");
+// Criando uma variável
+let mensagem = "Olá mundo!";
+console.log("Mensagem de variável: ", mensagem);
