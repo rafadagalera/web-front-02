96193a91813ffb022c965e6c8bd84db9ad32ebdd
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-20 11:10:43 -0300

    exemplos de for

---
+++ exemplo2.js
@@ -0,0 +1,9 @@
+const peças = ['rei','rainha','torre','bispo','cavalo','peão']
+
+for (let i = 0;i < peças.length; i++) {
+    console.log(peças[i]);
+}
+
+for (let i = 0;i < peças.length; i++) {
+    console.log(`${i} - ${peças[i]}`);
+}
---
+++ exemplo3.js
@@ -0,0 +1,5 @@
+const peças = ['rei','rainha','torre','bispo','cavalo','peão']
+
+for(let peça of peças) {
+    console.log(peça);
+}
