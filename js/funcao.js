496250e84262e9c5296400de1994b238e672d8e3
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-27 10:52:58 -0300

    funcoes

---
+++ js/funcao.js
@@ -0,0 +1,19 @@
+alert('Olá!')
+
+function teste(){
+    console.log('Olaaa');
+}
+
+function meuNome(name){
+    console.log(`Olá ${name}`);
+}
+function calc(arg1, arg2){
+    return arg1 + arg2;
+}
+meuNome('Carlos')
+meuNome('Sainz')
+meuNome('Operator')
+teste();
+console.log(calc(4,8));
+console.log(calc(4,10));
+console.log(calc(4,2));
